95d5ede224670fe259647186da563f9337f27d06
Elias Pöschl <eliaspoe194@gmail.com>
2025-04-02 20:02:21 +0200

    made base with colors and added Icon

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,16 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function MyComponent(props) {
-  return <h1>Hello, I am learning {props.name}</h1>;
+function MyComponent() {
+  return <h1>Hello, I am Delta</h1>;
 }
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
-      <MyComponent name="React"></MyComponent>
+      <MyComponent></MyComponent>
     </>
   )
 }
