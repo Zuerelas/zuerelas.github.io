69bb7b594dc75e3e5e33b109dd7737b6b9fdf19b
Elias Pöschl <eliaspoe194@gmail.com>
2025-07-10 19:33:41 +0200

    changed folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import Navigation from './nav.jsx'
 import MainText from './mainSection.jsx'
 import About from './about.jsx'
+import Projects from './projects.jsx'
+// import Contact from './contact.jsx'
 
 function App() {
   return (
---
+++ src/projects.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import "./App.css";
+
+function Projects() {
+
+}
+
+export default Projects; 1
